220ea882142ed1957e9eaddd7693126e86504ff4
ksanchez <kenrysanchez65@gmail.com>
2021-04-13 02:43:44 -0500

    updating data flow run.py

--- template/DataFlowRun.py
+++ template/DataFlowRun.py
@@ -29,14 +29,14 @@ class ReadFile(beam.DoFn):
                 for item in elems['object_annotations']:
 
                     start_time_seconds = item['segment']['start_time_offset'][
-                        'seconds'] if item['segment']['start_time_offset']['seconds'] != None else 0
+                        'seconds'] if hasattr(item, "seconds") else 0
                     start_time_nanos = item['segment']['start_time_offset'][
-                        'nanos'] if item['segment']['start_time_offset']['nanos'] != None else 0
+                        'nanos'] if hasattr(item, "nanos") else 0
 
                     end_time_seconds = item['segment']['end_time_offset'][
-                        'seconds'] if item['segment']['end_time_offset']['seconds'] != None else 0
-                    end_time_nanos = item['segment']['end_time_offset']['nanos'] if item[
-                        'segment']['end_time_offset']['nanos'] != None else 0
+                        'seconds'] if hasattr(item, "seconds") else 0
+                    end_time_nanos = item['segment']['end_time_offset']['nanos'] if hasattr(
+                        item, "nanos") else 0
 
                     yield {
                         'description': item['entity']['description'],
