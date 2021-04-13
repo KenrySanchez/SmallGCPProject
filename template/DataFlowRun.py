4d64f8a5651e1c651aeae8a915cf91791b634b4f
ksanchez <kenrysanchez65@gmail.com>
2021-04-13 16:54:46 -0500

    updating data flow run.py

--- template/DataFlowRun.py
+++ template/DataFlowRun.py
@@ -8,6 +8,7 @@ from apache_beam.options.value_provider import StaticValueProvider
 from google.cloud import storage
 from smart_open import open
 
+
 class ReadFile(beam.DoFn):
 
     def __init__(self, input_path):
@@ -17,35 +18,34 @@ class ReadFile(beam.DoFn):
         self.client = storage.Client()
 
     def process(self, something):
-        
+
         clear_data = []
         with open(self.input_path.get()) as line:
 
             jsonAsString = line.read()
 
             data = json.loads(jsonAsString)
-            
+
             for elems in data['annotation_results']:
                 for item in elems['object_annotations']:
 
                     start_time_seconds = item['segment']['start_time_offset'][
-                        'seconds'] if hasattr(item['segment']['start_time_offset'], "seconds") else 0
+                        'seconds'] if "seconds" in item['segment']['start_time_offset'] else 0
+                        
                     start_time_nanos = item['segment']['start_time_offset'][
-                        'nanos'] if hasattr(item['segment']['start_time_offset'], "nanos") else 0
-
-                    ## testing time. it seems it's not working!!
-                    logging.info("seconds" in item['segment']
-                                 ['start_time_offset'])
+                        'nanos'] if "nanos" in item['segment']['start_time_offset'] else 0
 
                     end_time_seconds = item['segment']['end_time_offset'][
-                        'seconds'] if hasattr(item['segment']['end_time_offset'], "seconds") else 0
-                    end_time_nanos = item['segment']['end_time_offset']['nanos'] if hasattr(
-                        item['segment']['end_time_offset'], "nanos") else 0
+                        'seconds'] if "seconds" in item['segment']['end_time_offset'] else 0
+
+                    end_time_nanos = item['segment']['end_time_offset'][
+                        'nanos'] if "seconds" in item['segment']['end_time_offset'] else 0
 
                     yield {
                         'description': item['entity']['description'],
                         'time': (end_time_seconds + end_time_nanos/(1*10**9)) - (start_time_seconds + start_time_nanos/(1*10**9))
                     }
+
 
 class DataflowOptions(PipelineOptions):
 
@@ -62,7 +62,8 @@ class DataflowOptions(PipelineOptions):
                 | 'Start' >> beam.Create([None])
                 | 'Read JSON' >> beam.ParDo(ReadFile(user_options.input))
                 | 'Write to BigQuery' >> beam.io.Write(beam.io.WriteToBigQuery('apt-subset-310017:bq_object_detection_store.data_store', schema="description:STRING,time:FLOAT", write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND))
-            )
+             )
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
